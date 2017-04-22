9c679554f14083780109c3f35dacdc90f6f40011
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-22 13:35:34 -0300

    Update chrome.sh

--- DugongGUI/ICEWM/.config/install/chrome.sh
+++ DugongGUI/ICEWM/.config/install/chrome.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Install Chromium Browser"
 apt-get update 
-apt-get install -y chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
+apt-get install -y --allow-unauthenticated chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg
 apt-get clean -y
 ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
