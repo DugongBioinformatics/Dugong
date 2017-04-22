570db91fbc34d3bb85c0252595cfc6db0a9c8f54
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-22 13:36:21 -0300

    Update icewm_ui.sh

--- DugongGUI/ICEWM/.config/install/icewm_ui.sh
+++ DugongGUI/ICEWM/.config/install/icewm_ui.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Install IceWM UI components"
 apt-get update 
-apt-get install -y supervisor icewm xterm xfonts-base xauth xinit
+apt-get install -y --allow-unauthenticated supervisor icewm xterm xfonts-base xauth xinit
 apt-get purge -y pm-utils xscreensaver*
 apt-get clean -y
