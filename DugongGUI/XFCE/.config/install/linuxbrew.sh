1fc10637fce82f84bda60304268b8a6042dae8ea
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-23 15:31:39 -0300

    Update linuxbrew.sh

--- DugongGUI/XFCE/.config/install/linuxbrew.sh
+++ DugongGUI/XFCE/.config/install/linuxbrew.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "Install Linuxbrew"
 git clone https://github.com/Linuxbrew/linuxbrew.git ~/.linuxbrew
-echo "export PATH=~/.linuxbrew/bin:$PATH" >> ~/.bashrc
-echo "export MANPATH=~/.linuxbrew/share/man:$MANPATH" >> ~/.bashrc
-echo "export INFOPATH=~/.linuxbrew/share/info:$INFOPATH" >> ~/.bashrc
+echo "PATH=$HOME/.linuxbrew/bin:$PATH" >> ~/.bashrc
+echo "export MANPATH=$HOME/.linuxbrew/share/man:$MANPATH" >> ~/.bashrc
+echo "export INFOPATH=$HOME/.linuxbrew/share/info:$INFOPATH" >> ~/.bashrc
+echo "PATH=$HOME/.linuxbrew/bin:$PATH" >> ~/.bash_profile
+echo "export MANPATH=$HOME/.linuxbrew/share/man:$MANPATH" >> ~/.bash_profile
+echo "export INFOPATH=$HOME/.linuxbrew/share/info:$INFOPATH" >> ~/.bash_profile
