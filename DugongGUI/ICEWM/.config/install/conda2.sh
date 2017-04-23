bf15f2ee9b3c333d25d2e5d4919fe1213bf2bff8
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-23 16:00:24 -0300

    Update conda2.sh

--- DugongGUI/ICEWM/.config/install/conda2.sh
+++ DugongGUI/ICEWM/.config/install/conda2.sh
@@ -7,6 +7,4 @@ echo "Install Miniconda 2"
 wget --quiet https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh && \
     /bin/bash Miniconda2-latest-Linux-x86_64.sh -b -p ~/.conda2 && \
     rm Miniconda2-latest-Linux-x86_64.sh && \
-    echo "export PATH=~/.conda2/bin:$PATH" >> ~/.bashrc && \
-    echo "export PATH=/headless/.conda2/bin:$PATH" >> /home/dugong/.bashrc && \
-    chown -R dugong:dugong /headless/.conda2
+    echo "export PATH=~/.conda2/bin:$PATH" >> ~/.bashrc
