d488936d45d217ee5713cb52b4d3eaa4c5b224bb
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-23 12:14:41 -0300

    Update linuxbrew.sh

--- DugongGUI/ICEWM/.config/install/linuxbrew.sh
+++ DugongGUI/ICEWM/.config/install/linuxbrew.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Linuxbrew/linuxbrew.git ~/.linuxbrew
 echo "export BREWPATH="~/.linuxbrew/bin:$BREWPATH"" >> ~/.bashrc
 echo "export MANPATH="~/.linuxbrew/share/man:$MANPATH"" >> ~/.bashrc
 echo "export INFOPATH="~/.linuxbrew/share/info:$INFOPATH"" >> ~/.bashrc
-echo "export BREWPATH="/root/.linuxbrew/bin:$BREWPATH"" >> /home/dugong/.bashrc
-echo "export MANPATH="/root/.linuxbrew/share/man:$MANPATH"" >> /home/dugong/.bashrc
-echo "export INFOPATH="/root/.linuxbrew/share/info:$INFOPATH"" >> /home/dugong/.bashrc
-chown -R dugong:dugong /root/.linuxbrew
+echo "export BREWPATH="/headless/.linuxbrew/bin:$BREWPATH"" >> /home/dugong/.bashrc
+echo "export MANPATH="/headless/.linuxbrew/share/man:$MANPATH"" >> /home/dugong/.bashrc
+echo "export INFOPATH="/headless/.linuxbrew/share/info:$INFOPATH"" >> /home/dugong/.bashrc
+chown -R dugong:dugong /headless/.linuxbrew
