8eb879645d089c9841ef316b94c7478db75ed1f2
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-24 10:31:54 -0300

    Update tools.sh

--- DugongCMD/.config/install/tools.sh
+++ DugongCMD/.config/install/tools.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
 
+echo "Install some common tools for further installation"
+apt-get update 
+apt-get install -y --allow-unauthenticated autotools-dev automake cmake curl make grep sed sudo dpkg fuse git wget zip \
+openjdk-8-jre build-essential pkg-config python python-dev python-pip bzip2 git libglib2.0-0 libxext6 libsm6 libxrender1 \ 
+mercurial zlib1g-dev openssh-server locales ca-certificates subversion vim 
+apt-get clean -y
