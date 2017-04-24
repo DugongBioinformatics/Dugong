ef5a36e463a0dc7a8b351477cc7bb0ef21cdb95f
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-24 10:35:23 -0300

    Create tini.sh

---
+++ DugongCMD/.config/install/tini.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+### every exit != 0 fails the script
+set -e
+
+echo "Install Tini"
+wget --quiet https://github.com/krallin/tini/releases/download/v0.10.0/tini && \
+    echo "1361527f39190a7338a0b434bd8c88ff7233ce7b9a4876f3315c22fce7eca1b0 *tini" | sha256sum -c - && \
+    mv tini /usr/local/bin/tini && \
+    chmod +x /usr/local/bin/tini
