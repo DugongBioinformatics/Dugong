c58ac1d244e689ce73fd2fb22aa06cdb1917f5bd
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-24 12:41:26 -0300

    Update tools.sh

--- DugongCMD/.config/install/tools.sh
+++ DugongCMD/.config/install/tools.sh
@@ -6,5 +6,5 @@ echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y --allow-unauthenticated make grep sed sudo dpkg git wget zip build-essential python python-dev python-pip \
 bzip2 mercurial openssh-server locales ca-certificates subversion vim gdebi-core bash-completion apt-utils software-properties-common \
-python-software-properties libgtk-3-bin libgtk-3-dev
+python-software-properties libgtk-3-0 libgtk-3-common libgtk-3-bin libgtk-3-dev
 apt-get clean -y
