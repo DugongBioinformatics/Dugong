922dcd2afd948f79294d64d732c8aabbe393d019
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-24 13:53:38 -0300

    Update tty.sh

--- DugongCMD/.config/install/tty.sh
+++ DugongCMD/.config/install/tty.sh
@@ -3,5 +3,10 @@
 set -e
 
 echo "Install tty.js"
-conda install nodejs npm
-npm install tty.js
+curl -sL https://deb.nodesource.com/setup_7.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get update 
+apt-get install -y --allow-unauthenticated nodejs
+apt-get clean -y
+
+
