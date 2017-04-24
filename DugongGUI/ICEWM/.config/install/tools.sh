4ac5bcb0fbf6a2c6ed021d0897841eee9582b476
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-24 12:27:01 -0300

    Update tools.sh

--- DugongGUI/ICEWM/.config/install/tools.sh
+++ DugongGUI/ICEWM/.config/install/tools.sh
@@ -6,5 +6,5 @@ echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y --allow-unauthenticated vim wget unzip curl git gdebi-core bash-completion build-essential ruby make net-tools \
 ca-certificates libglib2.0-0 libxext6 libsm6 libxrender1 mercurial subversion synaptic locales sudo apt-utils software-properties-common \ 
-python-software-properties libgtk-3-bin
+python-software-properties libgtk-3-bin libgtk-3-dev
 apt-get clean -y
