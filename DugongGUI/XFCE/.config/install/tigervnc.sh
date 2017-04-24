29a900b4ff2125fab5335313e226df0454955789
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-24 19:56:49 -0300

    Update tigervnc.sh

--- DugongGUI/XFCE/.config/install/tigervnc.sh
+++ DugongGUI/XFCE/.config/install/tigervnc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Install TigerVNC server"
-wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.7.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.7.0.x86_64.tar.gz | tar zxvf --strip 1 -C /
