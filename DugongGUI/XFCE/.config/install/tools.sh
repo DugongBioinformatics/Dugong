d2aed82c8c819abf74ae6bd0dd20cdcbbfda3089
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-26 22:50:54 -0300

    Update tools.sh

--- DugongGUI/XFCE/.config/install/tools.sh
+++ DugongGUI/XFCE/.config/install/tools.sh
@@ -3,7 +3,12 @@
 set -e
 
 echo "Install some common tools for further installation"
-apt-get update 
-apt-get install -y --allow-unauthenticated vim wget unzip curl git gdebi-core bash-completion build-essential ruby make net-tools \
-ca-certificates libglib2.0-0 libxext6 libsm6 libxrender1 mercurial subversion synaptic locales sudo apt-utils
-apt-get clean -y
+wget --quiet https://github.com/krallin/tini/releases/download/v0.10.0/tini && \
+echo "1361527f39190a7338a0b434bd8c88ff7233ce7b9a4876f3315c22fce7eca1b0 *tini" | sha256sum -c - && \
+mv tini /usr/local/bin/tini && chmod +x /usr/local/bin/tini && \
+bash -c "$(curl -L https://basemount.basespace.illumina.com/install)" && \
+wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync-xUbuntu_16.04_amd64.deb && \
+wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megacmd-xUbuntu_16.04_amd64.deb && \
+gdebi -n megasync-xUbuntu_16.04_amd64.deb && \
+gdebi -n megacmd-xUbuntu_16.04_amd64.deb && \
+rm megasync-xUbuntu_16.04_amd64.deb megacmd-xUbuntu_16.04_amd64.deb
