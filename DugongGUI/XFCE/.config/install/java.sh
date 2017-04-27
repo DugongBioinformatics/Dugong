5154cd2afa9a1ca4769be33aaabad846cef6626e
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-26 23:28:10 -0300

    Delete java.sh

--- DugongGUI/XFCE/.config/install/java.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-### every exit != 0 fails the script
-set -e
-
-echo "Install Java 8 and Icedtea Plugin 8"
-apt-get update 
-apt-get install -y --allow-unauthenticated openjdk-8-jre icedtea-8-plugin
-apt-get clean -y
