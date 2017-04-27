543384fe9be940dc0ebb5e7c57d360e25bc97879
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-26 23:55:05 -0300

    Delete basemount.sh

--- DugongGUI/ICEWM/.config/install/basemount.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-### every exit != 0 fails the script
-set -e
-
-echo "Install BaseMount, a FUSE driver that allows a BaseSpace user access to their data"
-bash -c "$(curl -L https://basemount.basespace.illumina.com/install)"
