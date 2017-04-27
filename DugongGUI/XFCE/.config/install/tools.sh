d902fea04190f222f75a61d5e93f091ab5296e86
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-04-26 22:54:07 -0300

    Update tools.sh

--- DugongGUI/XFCE/.config/install/tools.sh
+++ DugongGUI/XFCE/.config/install/tools.sh
@@ -6,7 +6,6 @@ echo "Install some common tools for further installation"
 wget --quiet https://github.com/krallin/tini/releases/download/v0.10.0/tini && \
 echo "1361527f39190a7338a0b434bd8c88ff7233ce7b9a4876f3315c22fce7eca1b0 *tini" | sha256sum -c - && \
 mv tini /usr/local/bin/tini && chmod +x /usr/local/bin/tini && \
-bash -c "$(curl -L https://basemount.basespace.illumina.com/install)" && \
 wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync-xUbuntu_16.04_amd64.deb && \
 wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megacmd-xUbuntu_16.04_amd64.deb && \
 gdebi -n megasync-xUbuntu_16.04_amd64.deb && \
