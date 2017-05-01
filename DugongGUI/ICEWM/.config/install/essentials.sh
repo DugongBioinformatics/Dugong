8825149867a03026e46b6e6a553c72e3647e99d2
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-05-01 18:18:34 -0300

    Update essentials.sh

--- DugongGUI/ICEWM/.config/install/essentials.sh
+++ DugongGUI/ICEWM/.config/install/essentials.sh
@@ -4,5 +4,5 @@ set -e
 echo "Install some common tools for further installation"
 apt-get update && apt-get install -y --allow-unauthenticated make grep sed sudo dpkg git wget zip build-essential python \
 python-dev python-pip bzip2 mercurial locales ca-certificates subversion vim gdebi-core bash-completion apt-utils openjdk-8-jre \
-openssh-server unzip curl ruby net-tools libglib2.0-0 libxext6 libsm6 libxrender1 aptitude icedtea-8-plugin synaptic \
-&& apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+openssh-server unzip curl ruby net-tools libglib2.0-0 libxext6 libsm6 libxrender1 aptitude icedtea-8-plugin synaptic zlib1g-dev \
+libncurses-dev && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
