f9a8f8b97f09e611e8956e0a87320b8fbe2bfd1f
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-05-02 00:01:25 -0300

    Update bioinformatics.sh

--- DugongGUI/XFCE/.config/install/bioinformatics.sh
+++ DugongGUI/XFCE/.config/install/bioinformatics.sh
@@ -4,9 +4,9 @@ set -e
 echo "Install some bioinformatics tools: Linuxbrew, Basemount and Bioconda"
 git clone https://github.com/Linuxbrew/linuxbrew.git ~/.linuxbrew && \
 bash -c "$(curl -L https://basemount.basespace.illumina.com/install)" && \
-wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
-/bin/bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/.conda3 && \
-rm Miniconda3-latest-Linux-x86_64.sh && \
-~/.conda3/bin/conda config --add channels r && \
-~/.conda3/bin/conda config --add channels bioconda && \
-~/.conda3/bin/conda clean -tipsy
+wget --quiet https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh && \
+/bin/bash Miniconda2-latest-Linux-x86_64.sh -b -p ~/.conda2 && \
+rm Miniconda2-latest-Linux-x86_64.sh && \
+~/.conda2/bin/conda config --add channels r && \
+~/.conda2/bin/conda config --add channels bioconda && \
+~/.conda2/bin/conda clean -tipsy
