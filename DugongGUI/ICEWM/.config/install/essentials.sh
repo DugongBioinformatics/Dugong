78a5295cdd9a25a03e6f5e2c1270cce2d1a002b2
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-05-02 03:20:12 -0300

    Update essentials.sh

--- DugongGUI/ICEWM/.config/install/essentials.sh
+++ DugongGUI/ICEWM/.config/install/essentials.sh
@@ -5,5 +5,5 @@ echo "Install some common tools for further installation"
 apt-get update && apt-get install -y --allow-unauthenticated make grep sed sudo dpkg git wget zip build-essential python \
 python-dev python-pip bzip2 mercurial locales ca-certificates subversion vim gdebi-core bash-completion apt-utils openjdk-8-jre \
 openssh-server unzip curl ruby net-tools libglib2.0-0 libxext6 libsm6 libxrender1 aptitude icedtea-8-plugin synaptic zlib1g-dev \
-pkg-config libncurses5-dev libbz2-dev lzma libcurl4-openssl-dev libxml2-dev libssl-dev libcairo2-dev pandoc \
+pkg-config libncurses5-dev libbz2-dev lzma libcurl4-openssl-dev libxml2-dev libssl-dev libcairo2-dev pandoc libgtextutils-dev \
 && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
