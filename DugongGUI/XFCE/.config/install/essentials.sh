d18f121b28dd15ceab53f7e833cec9715038a501
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-05-03 22:49:13 -0300

    Update essentials.sh

--- DugongGUI/XFCE/.config/install/essentials.sh
+++ DugongGUI/XFCE/.config/install/essentials.sh
@@ -3,7 +3,7 @@
 set -e
 echo "Install some common tools for further installation"
 apt-get update && apt-get install -y --allow-unauthenticated make grep sed sudo dpkg git wget zip build-essential python \
-python-dev python-pip bzip2 mercurial locales ca-certificates subversion vim gdebi-core bash-completion apt-utils openjdk-8-jre \
+python-dev python-pip  bzip2 mercurial locales ca-certificates subversion vim gdebi-core bash-completion apt-utils openjdk-8-jre \
 openssh-server unzip curl ruby net-tools libglib2.0-0 libxext6 libsm6 libxrender1 aptitude icedtea-8-plugin synaptic zlib1g-dev \
-pkg-config libncurses5-dev libbz2-dev python-lzma liblzma-dev cython libcurl4-openssl-dev libxml2-dev libssl-dev libgtextutils-dev libcairo2-dev pandoc \
-&& apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+pkg-config libncurses5-dev libbz2-dev python-lzma liblzma-dev cython libcurl4-openssl-dev libxml2-dev libssl-dev libgtextutils-dev \
+libcairo2-dev pandoc python-setuptools && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
