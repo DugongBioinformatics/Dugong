4624d2369ec08c9c59cd98cfca41f592fcbacf9a
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-07-01 14:50:16 -0300

    Update browsers.sh

--- DugongGUI/XFCE/.config/install/browsers.sh
+++ DugongGUI/XFCE/.config/install/browsers.sh
@@ -4,6 +4,7 @@ set -e
 echo "Install Chromium Browser" && \
 apt-get update && \
 apt-get install -y --allow-unauthenticated chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg && \
+apt-get autoremove -y && \
 apt-get clean -y && \
 ln -s /usr/bin/chromium-browser /usr/bin/google-chrome && \
 echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
