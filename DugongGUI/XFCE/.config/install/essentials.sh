9c04db0dfe96abe198102682def739646f005a19
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-07-01 01:24:29 -0300

    Update essentials.sh

--- DugongGUI/XFCE/.config/install/essentials.sh
+++ DugongGUI/XFCE/.config/install/essentials.sh
@@ -1,15 +1,8 @@
-
 #!/usr/bin/env bash
 ### every exit != 0 fails the script
 set -e
-
 echo "Install some common tools for further installation"
-wget --quiet https://github.com/krallin/tini/releases/download/v0.10.0/tini && \
-echo "1361527f39190a7338a0b434bd8c88ff7233ce7b9a4876f3315c22fce7eca1b0 *tini" | sha256sum -c - && \
-mv tini /usr/local/bin/tini && chmod +x /usr/local/bin/tini && \
-wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync-xUbuntu_16.04_amd64.deb && \
-wget https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megacmd-xUbuntu_16.04_amd64.deb && \
-gdebi -n megasync-xUbuntu_16.04_amd64.deb && \
-gdebi -n megacmd-xUbuntu_16.04_amd64.deb && \
-apt-get -f install && \
-rm megasync-xUbuntu_16.04_amd64.deb megacmd-xUbuntu_16.04_amd64.deb
+apt-get update && apt-get install -y --allow-unauthenticated make grep sed sudo dpkg git wget zip build-essential python \
+python-dev bzip2 locales ca-certificates vim gdebi-core bash-completion apt-utils openjdk-8-jre \
+openssh-server unzip curl ruby net-tools icedtea-8-plugin zlib1g-dev libqt4-dbus libqt4-network \
+&& apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
