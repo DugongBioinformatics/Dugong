6cf629212a5be8b87c3ba0daae20e41c885baee8
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-07-02 15:24:23 -0300

    Update wm_startup.sh

--- DugongGUI/LXDE/Ubuntu/.config/GUI/lxde/wm_startup.sh
+++ DugongGUI/LXDE/Ubuntu/.config/GUI/lxde/wm_startup.sh
@@ -2,13 +2,13 @@
 ### every exit != 0 fails the script
 set -e
 
-echo -e "\n------------------ startup of Xfce4 window manager ------------------"
+echo -e "\n------------------ startup of LXDE window manager ------------------"
 
 ### disable screensaver and power management
 xset -dpms &
 xset s noblank &
 xset s off &
 
-/usr/bin/startxfce4 --replace > $HOME/wm.log &
+/usr/bin/lxsession -s LXDE -e LXDE > $HOME/wm.log &
 sleep 1
 cat $HOME/wm.log
