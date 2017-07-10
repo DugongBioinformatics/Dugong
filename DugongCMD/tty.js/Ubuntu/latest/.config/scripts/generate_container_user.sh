4ea6393cf813c85593ba7c247e186f6db33e617f
Menegidio, F.B <fabianomenegidio@users.noreply.github.com>
2017-07-10 05:02:40 -0300

    Update generate_container_user.sh

--- DugongCMD/tty.js/Ubuntu/latest/.config/scripts/generate_container_user.sh
+++ DugongCMD/tty.js/Ubuntu/latest/.config/scripts/generate_container_user.sh
@@ -7,5 +7,6 @@ HOME=/headless
 echo "User: $USER Pass: $PASS"
 useradd -d $HOME --shell /bin/bash --user-group --groups adm,sudo $USER
 echo "$USER:$PASS" | chpasswd
-echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && \
+echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+mkdir $HOME/data
 chown -R $USER:$USER $HOME
